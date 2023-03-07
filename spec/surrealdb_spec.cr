7e5ef69dcc856456c67f9b53a895031e2fd3b48f
Muhammed Yaşar <ileti@yorci.com>
2023-03-07 12:55:44 +0300

    add basic tests

--- spec/surrealdb_spec.cr
+++ spec/surrealdb_spec.cr
@@ -1,9 +1,67 @@
 require "./spec_helper"
+require "../src/surrealdb"
 
-describe Surrealdb do
-  # TODO: Write tests
+guid = SurrealDB.guid
+sdb = SurrealDB.create(
+  url: "http://0.0.0.0:8000/sql",
+  client: SurrealDB::HTTP,
+)
 
-  it "works" do
-    false.should eq(true)
+describe SurrealDB do
+  it "connects" do
+    sdb.should be_a(SurrealDB::HTTP)
+  end
+
+  it "auth" do
+    sdb.signin "root", "root"
+    sdb.use "test", "test"
+    true.should be_true 
+  end
+
+  it "create" do
+    q = sdb.create(
+      "person",
+      {"title" => "Software Developer",
+       "name"  => {
+         "first" => "Muhammed",
+         "last"  => "Yaşar",
+       },
+       "marketing"  => true,
+       "identifier" => guid,
+      }
+    )
+    q.should be_a Array(SurrealDB::Response)
+  end
+
+  it "select" do
+    q = sdb.select("person")
+    q.should be_a Array(SurrealDB::Response)
+    q.as(Array(SurrealDB::Response)).first.result.as_a.empty?.should be_false
+  end
+
+  it "query" do
+    q = sdb.query(
+      %Q(SELECT identifier FROM person WHERE identifier = "#{guid}"),
+      nil
+    )
+    q.should be_a Array(SurrealDB::Response)
+    q.as(Array(SurrealDB::Response)).first.result[0]["identifier"].as_s.should eq guid
+  end
+
+  it "wrong query" do
+    q = sdb.query(
+      "SELECT marketing, counr() FROM person GROUP BY marketing",
+      nil
+    )
+    q.should be_a SurrealDB::ErrorResponse
+  end
+
+  it "delete" do
+    q = sdb.delete("person")
+    q.should be_a Array(SurrealDB::Response)
+
+    q2 = sdb.select("person")
+    q2.should be_a Array(SurrealDB::Response)
+    q2.as(Array(SurrealDB::Response)).first.result.as_a.empty?.should be_true
   end
 end
